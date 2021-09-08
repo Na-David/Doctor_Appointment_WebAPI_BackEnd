d917f33c4529598f021b963d828b51b79a795e0b
hna206@naver.com <hna206@naver.com>
2021-09-08 12:02:34 +1200

    Patient_Details

---
+++ Doctor_Appointment_WebAPI_BackEnd/Models/Patient_Details.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doctor_Appointment_WebAPI_BackEnd.Models
+{
+    public class Patient_Details
+    {
+        public int Id { get; set; }
+        public string Patient_Name { get; set; }
+        public string Patient_Mobile { get; set; }
+        public string Patient_Email { get; set; }
+        public string Patient_Address { get; set; }
+    }
+}
