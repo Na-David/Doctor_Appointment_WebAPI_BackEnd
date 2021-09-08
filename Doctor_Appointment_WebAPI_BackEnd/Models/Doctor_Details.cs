d4d8d8fc4c00def2efbba9590f4b774b64bb27e1
hna206@naver.com <hna206@naver.com>
2021-09-08 12:02:01 +1200

    Add project files.

---
+++ Doctor_Appointment_WebAPI_BackEnd/Models/Doctor_Details.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doctor_Appointment_WebAPI_BackEnd.Models
+{
+    public class Doctor_Details
+    {
+        public int Id { get; set; }
+        public string Doctor_Name { get; set; }
+        public string Doctor_Mobile { get; set; }
+        public string Doctor_Email { get; set; }
+        public string Doctor_Specialization { get; set; }
+
+    }
+}
