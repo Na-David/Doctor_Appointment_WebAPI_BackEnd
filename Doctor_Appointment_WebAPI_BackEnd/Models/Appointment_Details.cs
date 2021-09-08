87cc30dcd8e61ab65aaff5c9bc3371347d0f0b0c
hna206@naver.com <hna206@naver.com>
2021-09-08 12:03:12 +1200

    Appointment table added

---
+++ Doctor_Appointment_WebAPI_BackEnd/Models/Appointment_Details.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doctor_Appointment_WebAPI_BackEnd.Models
+{
+    public class Appointment_Details
+    {
+        public int Id { get; set; }
+        public Doctor_Details Doctor_ { get; set; }
+        public Patient_Details Patient_ { get; set; }
+        public DateTime Appointment { get; set; }
+    }
+}
